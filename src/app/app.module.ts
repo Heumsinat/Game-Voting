54b8bc0cb6dc68f2530a4431727d3cc57480c53c
Heum Sinat <heumsinatgic25@gmail.com>
2017-05-16 11:51:48 +0700

    First Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { Menu1Page } from '../pages/menu1/menu1';
+import { Menu2Page } from '../pages/menu2/menu2';
+import { Menu3Page } from '../pages/menu3/menu3';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    Menu1Page,
+    Menu2Page,
+    Menu3Page
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    Menu1Page,
+    Menu2Page,
+    Menu3Page
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ src/pages/home/home.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { AlertController, Platform, NavController, NavParams, LoadingController } from 'ionic-angular';
+import {Menu1Page} from '../menu1/menu1';
+import {Menu2Page} from '../menu2/menu2';
+import {Menu3Page} from '../menu3/menu3';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController, private alertCtrl: AlertController, private platform: Platform) {
+
+  }
+  menu1(){
+    this.navCtrl.push(Menu1Page);
+  }
+  menu2(){
+    this.navCtrl.push(Menu2Page);
+  }
+  menu3(){
+    this.navCtrl.push(Menu3Page);
+  }
+
+  exitButtonClick() {
+    let alert = this.alertCtrl.create({
+      title: 'ចាកចេញ',
+      message: 'តើ​អ្នក​ពិត​ជា​ចង់​ចាក​ចេញ​ពី​កម្មវិធី​នេះ?​',
+      buttons: [
+        {
+          text: "ទេ",
+          role: 'cancel'
+        },
+        {
+          text: "បាទ​ / ចាស",
+          handler: () => {
+            this.platform.exitApp();
+          }
+        },
+      ]
+    });
+    alert.present();
+  }
+
+}
